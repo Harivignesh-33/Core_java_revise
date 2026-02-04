029ab696d3e374e2e827bc161bf341077ec00c1e
Harivignesh-33 <harivigneshhari61@gmail.com>
2026-02-04 07:33:30 +0530

    updated methods_example

--- src/test/Methods_example.java
+++ src/test/Methods_example.java
@@ -11,11 +11,25 @@ public class Methods_example {
         System.out.println(a+b);
     }
 
+    void str(String a, String b){
+        System.out.println(a+b);
+    }
+
+    int add(int a,int b){
+        int c=a+b;
+        return c;
+    }
+
     public static void main(String[] args) {
         Methods_example caller=new Methods_example();
+        //  method without argument
         caller.addition();
-
-        Methods_example arg_method = new Methods_example();
-        arg_method.addition();
+//        method with argument
+        caller.addition(4,8);
+//        String method
+        caller.str("hello"," World");
+//        Return method
+        int x=caller.add(1,3);
+        System.out.println(x);
     }
 }
