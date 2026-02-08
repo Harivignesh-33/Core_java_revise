3ccc6da13d4657500c38b79dbdf1a3728c3af6a0
Harivignesh-33 <harivigneshhari61@gmail.com>
2026-02-08 18:57:06 +0530

    Array list

---
+++ src/test/ArrayListEx.java
@@ -0,0 +1,36 @@
+package test;
+
+import java.util.*;
+
+public class ArrayListEx {
+
+
+    public static void main(String[] args) {
+        ArrayList<String> names = new ArrayList<>();
+
+        //add elements to arraylist
+        names.add("ken");
+        names.add("john");
+        names.add("ban");
+//        Get particular element
+        System.out.println(names.get(1));
+//        replace particular index
+        names.set(1, "kin");
+        System.out.println("Arraylist : " + names);
+//        delete  by index
+        names.remove(1);
+        System.out.println(names);
+
+//        sort the array list
+        ArrayList<Integer> num = new ArrayList<>();
+        num.add(4);
+        num.add(2);
+        num.add(90);
+        num.add(3);
+//collection.sort() method  is used for all common lists  , Array list stores the data and collections  manages the data
+        Collections.sort(num);
+        System.out.println(num);
+
+    }
+
+}
