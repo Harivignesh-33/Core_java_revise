717bf95e25bd30df123a7dc393c94de975a85ffa
Harivignesh-33 <harivigneshhari61@gmail.com>
2026-02-11 07:47:15 +0530

    learnt interface & abstraction

---
+++ src/test/OOPS/I1.java
@@ -0,0 +1,43 @@
+package test.OOPS;
+
+public interface I1 {
+    void add();
+    void sub();
+    void mul();
+    void test();
+}
+interface I2{
+    void div();
+    void mod();
+}
+class Child implements I1 , I2{
+
+    public void add(){
+        System.out.println("Add");
+    }
+    public void sub(){
+        System.out.println("sub");
+    }
+    public void mul(){
+        System.out.println("mul");
+    }
+    public void test(){
+        System.out.println("Test");
+    }
+
+    public void mod(){
+        System.out.println("Mod");
+    }
+
+    public void div() {
+        System.out.println("div");
+    }
+
+    public static void main(String[] args) {
+        Child obj=new Child();
+        obj.add();
+        obj.sub();
+        obj.test();
+        obj.mul();
+    }
+}
